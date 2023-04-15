afe7483888d88cbf10533d81b484c685c02649cd
German Santos <santos.german@gmail.com>
2023-04-15 12:40:10 -0700

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+ 
+  if ((distance > 0) && (distance <= 400)) {
+    return('This one is on me!');    
+  }
+  else if ((distance > 400) && (distance <= 2000) ) {
+    return('That will be twenty bucks.');
+    }
+  
+    else if ((distance > 2000) && (distance <= 2500) ) {
+      return('I will gladly take your thirty bucks.');
+      }
+
+    else if (distance > 2500) {
+      return('No can do.');
+      }
+}
+  
+function ternaryCheckCity(city){
+  let cityReply;
+  city === 'NYC'? (cityReply = 'Ok, sounds good.') : (cityReply = 'No go.');
+  return(cityReply)
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipReply){
+  switch (tipReply) {
+    case 'generous':
+      tipReply = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipReply = 'Thank you.';
+      break;
+    default:
+      tipReply = 'Bye.';
+      break;    
+  }
+  return(tipReply);
 }
